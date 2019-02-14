7f9e83dd9d841694796e70601e37f5b11f964a4c
Maksym Hnoinskyi <mxmgny@vivaldi.net>
2019-02-14 12:36:22 +0200

    Fixed collision

--- js/app.js
+++ js/app.js
@@ -1,15 +1,28 @@
 const enemyPositionX = 0,
       enemyPositinY = 63, 
-      enemyRangeMax = 505;
-      enemyLoopStartPosition = -90;
-      enemyQuantity = 3;
+      enemyRangeMax = 505,
+      enemyWidth = 101,
+      enemyHeight = 171,
+      enemyLoopStartPosition = -90,
+      enemyQuantity = 3,
+      enemySpriteOffsetY = 81,
+      enemyCollisionHeight = 60,
+      heroSize = 81,
       heroPositionX = 202,
       heroPositionY = 400,
+      heroSpriteOffsetX = 11,
+      heroSpriteOffsetY = 60,
       heroMoveX = 101,
       heroMoveY = 83,
+      heroWidth = 101,
+      heroHeight = 171,
       heroMoveRange =[[-20,-20],
                       [505,450]];
+      var score=0;
+
+
     
+
 
 const getRandomSpeed = function() {
     return (Math.random()*250)+80;
@@ -19,7 +32,6 @@ const createEnemies = function(num){
     for(let i = 0; i < num;i++){
         enems.push(new Enemy(enemyPositionX,enemyPositinY+81*i, getRandomSpeed()));    
     }
-    console.log(enems);
     return enems;
 }
 
@@ -28,12 +40,14 @@ var Enemy = function(x,y,speed) {
     this.x  = x;
     this.y = y;
     this.speed = speed;
+    this.collisionY = this.y + enemySpriteOffsetY;
     this.sprite = 'images/enemy-bug.png';
 };
 
 Enemy.prototype.update = function(dt) {
     if(this.x>enemyRangeMax){
         this.x = enemyLoopStartPosition;
+        this.speed = getRandomSpeed();
     };
     this.x += this.speed * dt;
 };
@@ -46,11 +60,15 @@ Enemy.prototype.render = function() {
 var Player = function() {
     this.x = heroPositionX;
     this.y = heroPositionY;
+    this.collisionX = this.x + heroSpriteOffsetX;
+    this.collisionY = this.y + heroSpriteOffsetY;
     this.sprite = 'images/char-cat-girl.png';
 };
 Player.prototype.update = function(){
     this.x = this.x;
     this.y= this.y;
+    this.collisionX = this.x + heroSpriteOffsetX;
+    this.collisionY = this.y + heroSpriteOffsetY;
 };
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
@@ -101,3 +119,28 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
+
+var checkCollisions = function(){
+    var collides = false; 
+    if(player.y <68){
+        player.x = heroPositionX;
+        player.y = heroPositionY;
+        score +=1;
+        console.log( 'Your score: ' + score );
+    } 
+    
+    allEnemies.forEach(enemy => {
+        if(player.collisionX <= enemy.x + enemyWidth &&
+           player.collisionX + heroSize >= enemy.x &&
+           player.collisionY <= enemy.collisionY + enemyCollisionHeight &&
+           player.collisionY + heroSize >= enemy.collisionY) {
+           collides = true;
+        };
+    });
+    if(collides == true) {
+        score = 0;
+        player.x = heroPositionX;
+        player.y = heroPositionY;
+        console.log('You loose:c... Your score: ' + score);
+    }     
+}
